42d8c1c465d6b803f519ba4e7b2c74cc9d84d871
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-21 22:15:12 -0400

    360 Tour Upload

---
+++ data.js
@@ -0,0 +1,359 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-view-of-st-catherine-laboure-harrisburg-pa-exterior-1",
+      "name": "360 View of St. Catherine Laboure Harrisburg PA Exterior 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.09446581886277983,
+        "pitch": -0.027035890764059545,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5084805858397701,
+          "pitch": 0.04845311660491092,
+          "rotation": 0,
+          "target": "1-360-view-of-st-catherine-laboure-harrisburg-pa-exterior-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-360-view-of-st-catherine-laboure-harrisburg-pa-exterior-2",
+      "name": "360 View of St. Catherine Laboure Harrisburg PA Exterior 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.274684389636011,
+          "pitch": -0.01301140579221638,
+          "rotation": 0,
+          "target": "0-360-view-of-st-catherine-laboure-harrisburg-pa-exterior-1"
+        },
+        {
+          "yaw": 2.8003580304493187,
+          "pitch": -0.06713866369520538,
+          "rotation": 0,
+          "target": "2-360-view-of-st-catherine-laboure-harrisburg-pa-interior-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-360-view-of-st-catherine-laboure-harrisburg-pa-interior-1",
+      "name": "360 View of St. Catherine Laboure Harrisburg PA Interior 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.231444968599554,
+          "pitch": 0.04543558950843973,
+          "rotation": 0,
+          "target": "1-360-view-of-st-catherine-laboure-harrisburg-pa-exterior-2"
+        },
+        {
+          "yaw": 0.018303372917555905,
+          "pitch": 0.18305127632287643,
+          "rotation": 0,
+          "target": "3-360-view-of-st-catherine-laboure-harrisburg-pa-interior-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-360-view-of-st-catherine-laboure-harrisburg-pa-interior-2",
+      "name": "360 View of St. Catherine Laboure Harrisburg PA Interior 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1383838168548177,
+          "pitch": 0.14093680995549995,
+          "rotation": 0,
+          "target": "2-360-view-of-st-catherine-laboure-harrisburg-pa-interior-1"
+        },
+        {
+          "yaw": 0.004441056074355743,
+          "pitch": 0.2604429627956186,
+          "rotation": 0,
+          "target": "4-360-view-of-st-catherine-laboure-harrisburg-pa-interior-3"
+        },
+        {
+          "yaw": -0.005030665980816806,
+          "pitch": -0.4309618052978035,
+          "rotation": 0,
+          "target": "5-360-view-of-st-catherine-laboure-harrisburg-pa-interior-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-360-view-of-st-catherine-laboure-harrisburg-pa-interior-3",
+      "name": "360 View of St. Catherine Laboure Harrisburg PA Interior 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1390311373926725,
+          "pitch": 0.19624922888437268,
+          "rotation": 0,
+          "target": "3-360-view-of-st-catherine-laboure-harrisburg-pa-interior-2"
+        },
+        {
+          "yaw": 0.23731581452726758,
+          "pitch": -1.4419218629755814,
+          "rotation": 0,
+          "target": "5-360-view-of-st-catherine-laboure-harrisburg-pa-interior-4"
+        },
+        {
+          "yaw": 0.00000491804166813381,
+          "pitch": 0.14834768199766302,
+          "rotation": 0,
+          "target": "6-360-view-of-st-catherine-laboure-harrisburg-pa-interior-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-360-view-of-st-catherine-laboure-harrisburg-pa-interior-4",
+      "name": "360 View of St. Catherine Laboure Harrisburg PA Interior 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6553053010353072,
+          "pitch": 1.542743861931192,
+          "rotation": 0,
+          "target": "4-360-view-of-st-catherine-laboure-harrisburg-pa-interior-3"
+        },
+        {
+          "yaw": 3.1310193877782213,
+          "pitch": 0.4552017461922766,
+          "rotation": 0,
+          "target": "3-360-view-of-st-catherine-laboure-harrisburg-pa-interior-2"
+        },
+        {
+          "yaw": 0.0041079500086276255,
+          "pitch": 0.2910584907667406,
+          "rotation": 0,
+          "target": "6-360-view-of-st-catherine-laboure-harrisburg-pa-interior-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-360-view-of-st-catherine-laboure-harrisburg-pa-interior-5",
+      "name": "360 View of St. Catherine Laboure Harrisburg PA Interior 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.005452008022732713,
+          "pitch": -0.5351607406532128,
+          "rotation": 0,
+          "target": "5-360-view-of-st-catherine-laboure-harrisburg-pa-interior-4"
+        },
+        {
+          "yaw": -0.00324933215027734,
+          "pitch": 0.2520854148304785,
+          "rotation": 0,
+          "target": "4-360-view-of-st-catherine-laboure-harrisburg-pa-interior-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
